5f113452ea6978757af19ccdebe2ccd2e29dda84
MNF4691 <m_n_fonseka@homedepot.com>
2019-03-01 14:46:47 -0600

    Adding bidirectional streaming

--- GoClientServer/client/main.go
+++ GoClientServer/client/main.go
@@ -1,21 +1,36 @@
 package main
+
 import (
-  "log"
-  "countdown/api"
-  "golang.org/x/net/context"
-  "google.golang.org/grpc"
+	"GRPCThings/GoClientServer/api"
+	"fmt"
+	"log"
+
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
 )
+
 func main() {
-  var conn *grpc.ClientConn
-  conn, err := grpc.Dial(":7777", grpc.WithInsecure())
-  if err != nil {
-    log.Fatalf("did not connect: %s", err)
-  }
-  defer conn.Close()
-  c := api.NewPingClient(conn)
-  response, err := c.SayHello(context.Background(), &api.PingMessage{Greeting: "foo"})
-  if err != nil {
-    log.Fatalf("Error when calling SayHello: %s", err)
-  }
-  log.Printf("Response from server: %s", response.Greeting)
+	var conn *grpc.ClientConn
+
+	conn, err := grpc.Dial(":7777", grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("did not connect: %s", err)
+	}
+	defer conn.Close()
+
+	c := api.NewPingClient(conn)
+	response, err := c.SayHello(context.Background(), &api.PingMessage{Greeting: "foo"})
+	if err != nil {
+		log.Fatalf("Error when calling SayHello: %s", err)
+	}
+	log.Printf("Response from Ping: %s", response.Greeting)
+
+	h := api.NewHealthClient(conn)
+
+	fmt.Println(h)
+	healthCheck, err := h.HealthCheck(context.Background(), &api.HealthMessage{})
+
+	fmt.Println(err)
+	log.Printf("Response from health : %s \n", healthCheck.Health)
+
 }
