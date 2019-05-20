bce62f10ac550599c9369cc24d2e10c86765c484
Niraj-Fonseka <fonseka.live@utexas.edu>
2019-05-20 01:45:42 -0500

    Adding the python client and server got some more things to do

---
+++ PythonClientServer/client.py
@@ -0,0 +1,43 @@
+# Copyright 2015 gRPC authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""The Python implementation of the gRPC route guide client."""
+
+from __future__ import print_function
+
+import random
+import logging
+
+import grpc
+
+import api_pb2
+import api_pb2_grpc
+
+def hello(stub):
+    msg =  api_pb2.PingMessage()
+
+    stub.SayHello(msg)
+
+def run():
+    # NOTE(gRPC Python Team): .close() is possible on a channel and should be
+    # used in circumstances in which the with statement does not fit the needs
+    # of the code.
+    with grpc.insecure_channel('localhost:50051') as channel:
+        stub = api_pb2_grpc.PingStub(channel)
+        print("-------------- Say Hello --------------")
+        hello(stub)
+
+
+if __name__ == '__main__':
+    logging.basicConfig()
+    run()
